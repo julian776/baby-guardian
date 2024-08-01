db9a0a8bf3f7a4a546ad23b8a6499c3fd908afad
Julian Alvarez <juliancamiloalvarez77@gmail.com>
2024-07-31 21:56:35 -0500

    Add AWS credentials provider function

--- apps/monitor/go.mod
+++ apps/monitor/go.mod
@@ -2,12 +2,24 @@ module github.com/julian776/baby-guardian/monitor
 
 go 1.22.3
 
+require github.com/stretchr/testify v1.9.0
+
 require (
-	github.com/stretchr/testify v1.9.0
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.15 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.15 // indirect
+	github.com/aws/smithy-go v1.20.3 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.30.3
+	github.com/aws/aws-sdk-go-v2/service/kinesis v1.29.3
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rs/zerolog v1.33.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ apps/monitor/pkg/aws/credentials.go
@@ -0,0 +1,60 @@
+package aws
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+)
+
+var (
+	errInvalidCredentialsFile = errors.New("invalid credentials file")
+)
+
+func CredentialsProvider(filePath string) aws.CredentialsProvider {
+	return aws.NewCredentialsCache(aws.CredentialsProviderFunc(credentialsProvider(filePath)))
+}
+
+func credentialsProvider(filePath string) func(context.Context) (aws.Credentials, error) {
+	return func(ctx context.Context) (aws.Credentials, error) {
+		file, err := os.Open(filePath)
+		if err != nil {
+			return aws.Credentials{}, err
+		}
+		defer file.Close()
+
+		accessKey := ""
+		secretKey := ""
+
+		// Read credentials from file
+		r := bufio.NewReader(file)
+		for i := 0; i < 2; i++ {
+			bytes, err := r.ReadBytes('\n')
+			if err != nil {
+				return aws.Credentials{}, err
+			}
+
+			// Parse credentials
+			s := strings.Split(string(bytes), "=")
+			if len(s) != 2 {
+				return aws.Credentials{}, errInvalidCredentialsFile
+			}
+
+			if strings.Contains(s[0], "access") {
+				accessKey = strings.TrimSpace(s[1])
+			} else if strings.Contains(s[1], "secret") {
+				secretKey = strings.TrimSpace(s[1])
+			} else {
+				return aws.Credentials{}, errInvalidCredentialsFile
+			}
+		}
+
+		return aws.Credentials{
+			AccessKeyID:     accessKey,
+			SecretAccessKey: secretKey,
+		}, nil
+	}
+}
