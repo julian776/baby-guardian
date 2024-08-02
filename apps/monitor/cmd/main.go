04d5a9a2d39ceb9c02bed99744af7efc16a3a63c
Julian Alvarez <juliancamiloalvarez77@gmail.com>
2024-08-01 20:15:35 -0500

    Add main app

--- apps/monitor/cmd/main.go
+++ apps/monitor/cmd/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/kinesis/types"
+	"github.com/julian776/baby-guardian/monitor/internal/monitor"
+	"github.com/julian776/baby-guardian/monitor/pkg/sensors"
+	"github.com/julian776/baby-guardian/monitor/pkg/streamers"
+	"github.com/rs/zerolog"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	rootCtx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
+
+	streamer := streamers.NewKinesis(
+		&streamers.KinesisConfig{
+			StreamName: "baby-guardian",
+			Region:     "us-east-1",
+			StreamMode: types.StreamModeOnDemand,
+		},
+		&logger,
+	)
+
+	monitor := monitor.NewMonitor(
+		streamer,
+		&logger,
+	)
+	monitor.AddSensors(
+		sensors.NewAudio(
+			"audio",
+			time.Second*5,
+		),
+	)
+
+	err := monitor.Start(rootCtx)
+	if err != nil {
+		logger.Panic().Err(err).Msg("failed to start monitor")
+	}
+
+	<-rootCtx.Done()
 }
--- apps/monitor/pkg/sensors/signal.go
+++
@@ -1,9 +0,0 @@
-package sensors
-
-import "time"
-
-type Signal struct {
-	Type      string
-	Timestamp time.Time
-	Value     float64
-}
