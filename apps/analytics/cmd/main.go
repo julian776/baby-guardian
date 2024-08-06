3c1aa3f80efb2a03321a1c762adb7ba03dc80e9d
Julian Alvarez <juliancamiloalvarez77@gmail.com>
2024-08-06 07:54:54 -0500

    Add monitorer to analytics

--- apps/analytics/cmd/main.go
+++ apps/analytics/cmd/main.go
@@ -5,6 +5,12 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/aws/aws-sdk-go-v2/service/kinesis/types"
+	"github.com/julian776/baby-guardian/analytics/internal/analyzer"
+	"github.com/julian776/baby-guardian/analytics/internal/monitor"
+	"github.com/julian776/baby-guardian/analytics/pkg/alerts"
+	"github.com/julian776/baby-guardian/monitor/pkg/streamers"
+	pb "github.com/julian776/baby-guardian/protos"
 	"github.com/rs/zerolog"
 )
 
@@ -15,6 +21,45 @@ func main() {
 	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
 
 	logger.Info().Msg("analyzing baby data")
+
+	streamer := streamers.NewKinesis(
+		&streamers.KinesisConfig{
+			StreamName: "baby-guardian",
+			Region:     "us-east-1",
+			StreamMode: types.StreamModeOnDemand,
+		},
+		&logger,
+	)
+
+	a := analyzer.NewAnalyzer(
+		&logger,
+	)
+	a.AddThreshold(
+		pb.Type_TEMPERATURE,
+		analyzer.Threshold{
+			Min: 15,
+			Max: 22,
+		},
+	)
+	a.AddThreshold(
+		pb.Type_AUDIO,
+		analyzer.Threshold{
+			Min: 0,
+			Max: 10,
+		},
+	)
+
+	monitor := monitor.NewMonitor(
+		streamer,
+		a,
+		&logger,
+		alerts.NewConsole(&logger),
+	)
+
+	err := monitor.Start(rootCtx)
+	if err != nil {
+		logger.Panic().Err(err).Msg("failed to start monitor")
+	}
 
 	<-rootCtx.Done()
 }
---
+++ apps/analytics/internal/monitor/monitor.go
@@ -0,0 +1,92 @@
+package monitor
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/julian776/baby-guardian/analytics/internal/analyzer"
+	"github.com/julian776/baby-guardian/analytics/pkg/alerts"
+	"github.com/julian776/baby-guardian/monitor/pkg/streamers"
+	pb "github.com/julian776/baby-guardian/protos"
+	"github.com/rs/zerolog"
+	"google.golang.org/protobuf/proto"
+)
+
+type Monitor struct {
+	streamer streamers.Streamer
+	analyzer *analyzer.Analyzer
+	alerts   []alerts.Alerter
+	logger   *zerolog.Logger
+}
+
+func NewMonitor(
+	streamer streamers.Streamer,
+	analyzer *analyzer.Analyzer,
+	logger *zerolog.Logger,
+	alerts ...alerts.Alerter,
+) *Monitor {
+	return &Monitor{
+		streamer: streamer,
+		analyzer: analyzer,
+		alerts:   alerts,
+		logger:   logger,
+	}
+}
+
+func (m *Monitor) Start(ctx context.Context) error {
+	if s, ok := m.streamer.(streamers.Starter); ok {
+		if err := s.Start(ctx); err != nil {
+			return fmt.Errorf("failed to start streamer: %w", err)
+		}
+	}
+
+	return m.consumue(ctx)
+}
+
+func (m *Monitor) consumue(ctx context.Context) error {
+	// Create a new context for the sensor restart
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	for {
+		recs, err := m.streamer.GetRecords(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to get records: %w", err)
+		}
+
+		for _, rec := range recs {
+			select {
+			case <-ctx.Done():
+				return nil
+			default:
+			}
+
+			signal := &pb.Signal{}
+			err := proto.Unmarshal(rec, signal)
+			if err != nil {
+				m.logger.Error().Err(err).Msg("failed to unmarshal signal")
+				continue
+			}
+
+			m.logger.Info().Msgf("received signal: %s", signal.String())
+
+			r := m.analyzer.Analyze(signal)
+			if r.IsDangerous() {
+				m.alert(r.String())
+			}
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+}
+
+func (m *Monitor) alert(message string) {
+	for _, alert := range m.alerts {
+		err := alert.Alert(message)
+		if err != nil {
+			m.logger.Error().Err(err).Msg("failed to alert: " + message)
+		}
+	}
+}
